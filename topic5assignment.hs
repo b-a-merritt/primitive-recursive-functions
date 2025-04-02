2f2552da7bb912d79320cd11510f1deb6aab6b66
Ben <ben.a.merritt@gmail.com>
2025-04-02 14:53:41 +0300

    add example of function

--- topic5assignment.hs
+++ topic5assignment.hs
@@ -1,3 +1,7 @@
 main = do
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
+  putStrLn ("Sum: " ++ show (add 5 4))
+
+add :: Integer -> Integer -> Integer
+add x y = x + y
